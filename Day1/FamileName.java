23ba9252401e2017bd063fa5a5b6b33a049741d0
hikvladimir <psihouse10@gmail.com>
2024-10-05 15:04:42 +0300

    task1

---
+++ Day1/FamileName.java
@@ -0,0 +1,12 @@
+/*Создать новый класс, который буддет выводит
+* имен членов вашей семьи, каждое с новой
+* строки. Придумать классу соответствующее
+* названия */
+public class FamileName {
+    public static void main(String[] args) {
+        System.out.println("Ольга");
+        System.out.println("Serge");
+        System.out.println("Владимир");
+        System.out.println("Любовь");
+    }
+}
