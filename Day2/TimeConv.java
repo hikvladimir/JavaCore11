4405525b41c8d8909e80a801d429e77c4e0f40a6
hikvladimir <psihouse10@gmail.com>
2024-10-07 23:46:33 +0300

    time

---
+++ Day2/TimeConv.java
@@ -0,0 +1,18 @@
+/*Создать новый класс, в котором вы
+* вводите количество секунд, а программа
+* выводит в консоль, сколько это дней, часов,
+* минут и секунд*/
+public class TimeConv {
+    public static void main(String[] args) {
+        int time=500000;
+        int sec=time%60;
+        int min=time/60;
+        int hour=min/60;
+        int day=hour/24;
+        System.out.println("Количество секунд: "+sec);
+        System.out.println("Количество минут: "+min);
+        System.out.println("Количество часов: "+hour);
+        System.out.println("Количество дней: "+day);
+
+    }
+}
